43af3989aa328a27dc91b90e862dd0d6bd0b0a92
Ahmad Ali <ahmad.ali.siddiq.bukhari@gmail.com>
2021-12-27 23:50:03 +0500

    Added Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# user = User.create!(email: 'ahmad@example.com', password: 'password')
+admin = User.create(name: 'Admin', email: 'admin@example.com', password: 'password')
+customer = User.create(name: 'Customer', email: 'customer@example.com', password: 'password')
+region = Region.create(title: 'South America', country: 'USA', currency: 'USD', tax: 10.10, admin: admin)
+Store.create(name: 'Carrefour', region: region, admin: admin)
+
+products = [
+ { title: Faker::Book.title, description: Faker::Movies::BackToTheFuture.quote, price: 100.0, sku: Faker::Name.unique.name, stock: 100, region_id: region.id, image_url: Faker::Internet.url},
+ { title: Faker::Book.title, description: Faker::Movies::BackToTheFuture.quote, price: 200.0, sku: Faker::Name.unique.name, stock: 100, region_id: region.id, image_url: Faker::Internet.url},
+ { title: Faker::Book.title, description: Faker::Movies::BackToTheFuture.quote, price: 300.0, sku: Faker::Name.unique.name, stock: 100, region_id: region.id, image_url: Faker::Internet.url}
+]
+
+Product.create(products)
